1d2f75eb3c9474465b7dcde0f1962f576e0b4bd5
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2025-02-14 01:56:45 +0900

    update

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 const { PrismaClient } = require('@prisma/client');
 const express = require('express');
 const app = express();
-const { PrismaClient } = require('@prisma/client');
+const bycrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const PORT = 5000;
 
 const prisma = new PrismaClient();
+
+app.use(express.json());
 
 app.get('/', async(req, res) => { 
   res.send('<h1>Hello World</h1>');
@@ -20,9 +24,35 @@ app.get('/', async(req, res) => {
 });
 
 // 新規ユーザー登録API
-app.post("/api/auth/register", (req, res) => { 
+app.post("/api/auth/register",async (req, res) => { 
   const { username, email, password } = req.body;
-  const user = await 
+  const hashedPassword = await bycrypt.hashSync(password, 10);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    }
+  });
+  return res.json({ user });
+});
+
+// ユーザーログインAPI
+app.post('/api/auth/login', async (req, res) => { 
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(401).json({ message: 'User not found' });
+  }
+
+  const isPasswordValid = await bycrypt.compare(password, user.password);
+  
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid password' });
+  }
+  
+  const token = jwt.sign({ id: user.id }, 'SECRET_KEY', { expiresIn: '1d' }, process);
+  return res.json({ token });
 });
 
 
